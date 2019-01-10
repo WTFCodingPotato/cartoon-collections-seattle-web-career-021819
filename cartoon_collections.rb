4d980bfaad72150fa6e80e88c22bebd4e439405a
Joshua M Loran <joshua.loran@gmail.com>
2019-01-10 03:46:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,8 @@ def long_planeteer_calls(array)
   array.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(some_cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.include?(some_cheese)
 end
